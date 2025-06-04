3ad377c377344c3b813a24ffac1db11363e4ea65
anxyum <115811968+anxyum@users.noreply.github.com>
2025-06-04 11:58:59 +0200

    saucisson

--- js/script.js
+++ js/script.js
@@ -2,25 +2,76 @@ const $burgerMenuButton = document.querySelector(
   ".hero-header-burger-menu-btn"
 );
 const $nav = document.querySelector(".hero-header-nav");
-const $heroForm = document.querySelector(".hero-form");
+
+const $heroForm = document.querySelector(".short-links-form");
 const $heroFormInputs = {
-  link: document.getElementById("hero-form-link-input"),
+  link: document.getElementById("short-links-form-link-input"),
 };
+
+const $shortenLinks = document.querySelector(".short-links-shortened-links");
 
 console.log($burgerMenuButton);
 console.log($nav);
+
 console.log($heroForm);
+console.log($heroFormInputs);
+
+console.log($shortenLinks);
 
 async function shortenLink(url) {
-  const res = await fetch("http://10.59.122.39:3000/shorten", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({ url }),
+  try {
+    const res = await fetch("http://10.59.122.39:3000/shorten", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+    const data = res.json();
+    return data;
+  } catch {
+    return "aaa";
+  }
+}
+
+function displayNewShortenLink(link, shortenLink) {
+  let newShortenLink = document.createElement("div");
+  newShortenLink.innerHTML = `<li class="short-links-shortened-link">
+    <div class="short-links-shortened-link-links-wrapper">
+      <a
+        class="short-links-shortened-link-link"
+        href="${link}"
+      >
+        ${link}
+      </a>
+      <hr class="short-links-shortened-link-separator" />
+      <a
+        class="short-links-shortened-link-short-link"
+        href="${shortenLink}"
+      >
+        ${shortenLink}
+      </a>
+    </div>
+    <button class="btn short-links-shortened-link-copy-btn">Copy</button>
+    <button class="btn short-links-shortened-link-copy-btn--copied hidden">
+      Copied!
+    </button>
+  </li>`;
+  newShortenLink = newShortenLink.querySelector("*");
+
+  const $copyBtn = newShortenLink.querySelector(
+    ".short-links-shortened-link-copy-btn"
+  );
+  const $copiedBtn = newShortenLink.querySelector(
+    ".short-links-shortened-link-copy-btn--copied"
+  );
+  $copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(shortenLink);
+    $copyBtn.classList.add("hidden");
+    $copiedBtn.classList.remove("hidden");
   });
-  const data = res.json();
-  return data;
+
+  $shortenLinks.appendChild(newShortenLink);
 }
 
 $burgerMenuButton.addEventListener("click", () => {
@@ -39,5 +90,5 @@ $heroForm.addEventListener("submit", async (e) => {
   $heroFormInputs.link.parentElement.classList.remove("error");
 
   const shortLink = await shortenLink(link);
-  console.log(shortLink);
+  displayNewShortenLink(link, shortLink);
 });
